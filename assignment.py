c42e73de3a2f88841c4a56aeaa40b37349a122d3
sajvanwijk <sebas-vanwijk@outlook.com>
2021-04-06 11:08:25 +0200

    Added grid search regularization

--- assignment.py
+++ assignment.py
@@ -30,7 +30,7 @@ scaler.fit_transform(x)
 # ----------------------------------
 from sklearn.decomposition import PCA
 
-def perform_pca(train_df, validation_df):
+def perform_pca(train_df, validation_df): # TODO hoor je dit niet alleen op de trainingsset te doen? Maar hoe?
     # Perform PCA
     pca = PCA(n_components = .90)
     principal_components_train = pca.fit_transform(train_df)
@@ -48,27 +48,27 @@ assert all(x) <= 1
 # ----------
 # Classifier
 # ----------
-def classifier(x_train, x_test, y_train, y_test):
-    # Begin by importing all necessary libraries
-    import pandas as pd
-    from sklearn.metrics import classification_report
-    from sklearn.metrics import confusion_matrix
-    from sklearn.metrics import accuracy_score
-    from sklearn.neighbors import KNeighborsClassifier
-    from sklearn.svm import SVC
-    from sklearn.linear_model import LogisticRegression
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn.ensemble import RandomForestClassifier
-    from sklearn.naive_bayes import GaussianNB
-    from sklearn.model_selection import train_test_split
+# Begin by importing all necessary libraries
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
 
-    SVC_model = SVC()
-    KNN_model = KNeighborsClassifier(n_neighbors=10)
-    LG_model = LogisticRegression(max_iter=10000)
-    DTR_model = DecisionTreeClassifier()
-    RFC_model = RandomForestClassifier()
-    GNB_model = GaussianNB()
+SVC_model = SVC()
+KNN_model = KNeighborsClassifier(n_neighbors=10)
+LG_model = LogisticRegression(max_iter=10000)
+DTR_model = DecisionTreeClassifier()
+RFC_model = RandomForestClassifier()
+GNB_model = GaussianNB()
 
+def fit_classifier(x_train, x_test, y_train, y_test):
     SVC_model.fit(x_train, y_train)
     KNN_model.fit(x_train, y_train)
     LG_model.fit(x_train, y_train)
@@ -104,10 +104,10 @@ k = 10
 skf = StratifiedKFold(n_splits = k, shuffle = True)
 all_pred_accuracies = {}
 for train_index, test_index in skf.split(x, y):
-    [predictions, pred_accuracies, pred_metrics] = classifier(x.iloc[train_index],
-                                                              x.iloc[test_index],
-                                                              y.iloc[train_index],
-                                                              y.iloc[test_index])
+    [predictions, pred_accuracies, pred_metrics] = fit_classifier(x.iloc[train_index],
+                                                                  x.iloc[test_index],
+                                                                  y.iloc[train_index],
+                                                                  y.iloc[test_index])
 
     if all_pred_accuracies == {}:  # Initialize the dict that's going to hold all predictions
         all_pred_accuracies = pred_accuracies.copy()
@@ -125,8 +125,36 @@ for pred_type in all_pred_accuracies:
     print(f'{pred_type}: {np.mean(all_pred_accuracies[pred_type])}')
 
 # --------------------------
+# Grid Search Regularization
+# --------------------------
+run_grid_search = True
+if run_grid_search:
+    def grid_search_reg(model, params):
+        from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold
+        from sklearn.utils import parallel_backend
+
+        search = GridSearchCV(
+            estimator=model, param_grid=params, scoring='accuracy', cv=3
+        )
+        with parallel_backend('threading'):
+            search.fit(x, y)
+
+        reg_results = pd.DataFrame(search.cv_results_)
+        reg_results = reg_results.sort_values(by=['rank_test_score'])
+        return reg_results
+
+    params_SVC = {'C': [0.1, 1, 10, 100, 1000],  
+              'gamma': [1, 0.1, 0.01, 0.001, 0.0001], 
+              'kernel': ['rbf','linear','sigmoid']}
+    params_RFC = {'n_estimators': [10,50,100],
+                  'min_samples_split': [1,2,5]}
+    reg_results = grid_search_reg(RFC_model, params_RFC)
+    print(reg_results)
+
+# --------------------------
 # Final test on test dataset
 # --------------------------
+
 run_final_test = False
 if run_final_test:
     [predictions, pred_accuracies, pred_metrics] = classifier(x_train, x_test, y_train, y_test)
